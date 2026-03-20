6805d46a072f68ee0aef534e7cf5560b075e6125
Nguyen284 <nguyenhoang280004@gmail.com>
2026-03-20 09:33:58 +0700

    [Git] - Fix xung đột 2 nhánh Login & Cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,15 @@ import { Routes } from '@angular/router';
 import { CartComponent } from './features/cart/cart.component/cart.component';
 
 export const routes: Routes = [
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  // AUTH
+  {
+    path: 'login',
+    loadComponent: () => import('./auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./auth/register/register.component').then((m) => m.RegisterComponent),
+  },
 ];
 
