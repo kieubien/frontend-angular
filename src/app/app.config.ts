08b513da9ab68914def14b27742fa33431eba5e3
HuynhTran-KhangHy <khanghy.huynhtran031006@gmail.com>
2026-03-20 07:00:33 +0700

    [Client]-Cart (features/cart/cart.component)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    importProvidersFrom(FormsModule) // để dùng [(ngModel)] trong CartComponent
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './features/cart/cart.component/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'cart', component: CartComponent }
+];
+
