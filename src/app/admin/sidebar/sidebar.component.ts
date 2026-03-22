a992524c23342d43f8977b2f53944ea5038430f6
HuynhTran-KhangHy <khanghy.huynhtran031006@gmail.com>
2026-03-22 23:02:15 +0700

    [HTML][Admin] - Fix chuyển trang - routerLink(Đơn hàng, Sản phẩm)

--- src/app/admin/sidebar/sidebar.component.ts
+++ src/app/admin/sidebar/sidebar.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-sidebar',
+  standalone: true, 
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  imports: [RouterModule] 
 })
 export class SidebarComponent {}
