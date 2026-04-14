19e78074a6ae2914080fd6e13f41c7931103a0d9
Nguyen284 <nguyenhoang280004@gmail.com>
2026-04-14 22:42:46 +0700

    [Database] - Kết nối hosting

--- BE/sync.js
+++ BE/sync.js
@@ -1,1 +1,14 @@
-process.chdir(__dirname); const sequelize = require('./database'); const Category = require('./models/category'); sequelize.sync({ alter: true }).then(() => console.log('OK')).catch(console.log);
+process.chdir(__dirname); 
+const sequelize = require('./database'); 
+const Category = require('./models/category'); 
+const User = require('./models/user'); 
+
+sequelize.sync({ alter: true })
+  .then(() => {
+    console.log('Đã đồng bộ cấu trúc bảng thành công!');
+    process.exit();
+  })
+  .catch((err) => {
+    console.error('Lỗi đồng bộ:', err);
+    process.exit(1);
+  });
