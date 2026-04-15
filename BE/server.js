79968f18ecad852422b91a78a618e0e72b8e2d58
Trần Hoàng Nguyện <nguyenhoang280004@gmail.com>
2026-04-15 16:45:26 +0700

    triển khai máy chủ phụ trợ và chế độ xem chi tiết sản phẩm và trang chủ ở giao diện người dùng

--- BE/server.js
+++ BE/server.js
@@ -11,7 +11,8 @@ const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 app.use(compression());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors({
     origin: "*",
